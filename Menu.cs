5c30d7d03c0e84da16ba375ad6185fb6523fa23a
solrave <demidovsergey777@gmail.com>
2024-10-07 18:46:17 +0200

    Added enum for menu options. Added MenuHandler method. SwitchMenu() is refactored. enum as argument. Needs a replace part logic to be added.+

--- Menu.cs
+++ Menu.cs
@@ -1,25 +1,30 @@
 namespace CarService2;
 using static System.Console;
+
+enum CurrentMenu
+{
+    MainMenu, CarMenu, PartMenu, StorageMenu
+}
 public class Menu
 {
     private delegate void MenuAction();
-    private List<string> _menuOptions;
+    private List<string> _mainMenu;
     private Dictionary<string, MenuAction> _menuActions;
     private int _menuIndexLower;
     private int _menuIndexUpper;
-    private int _currentMenuIndex;
-    private MarketGenerator _street;
+    private int _menuObject;
+    private MarketGenerator _clientList;
     private CarService _carService;
     private Storage _storage;
     private Car CarToRepair;
     public Menu(MarketGenerator street, CarService carService, Storage storage)
     {
-        _menuOptions = new() { { "Start the Work" }, { "Show the Storage" }, { "Exit the Program" } };
-        _street = street;
+        _mainMenu = new() { { "Start the Work" }, { "Show the Storage" }, { "Exit the Program" } };
+        _clientList = street;
         _carService = carService;
         _storage = storage;
         _menuIndexLower = 0;
-        _currentMenuIndex = 0;
+        _menuObject = 0;
         _menuActions = new Dictionary<string, MenuAction>
     {
         {"Start the Work", new (this.StartWork)},
@@ -36,58 +41,84 @@ public class Menu
 
     public void RunMenu()
     {
-        ShowMenu(_menuOptions);
-        SwitchMenu(_menuOptions);
-    }
-    
-    
-    private void ShowMenu<T>(List<T> list)
-    {
-        ClearConsole();
-        _menuIndexUpper = list.Count;
-        for (int i = 0; i < _menuIndexUpper; i++)
-        {
-            WriteLine(_currentMenuIndex == i 
-                ? $">>{list[i]}<<"
-                : $"{list[i]}");
-        }
+        MenuHandler(CurrentMenu.MainMenu);
+        SwitchMenu(CurrentMenu.MainMenu);
         
     }
     
-    private void SwitchMenu<T>(List<T> list)
+    
+    private void MenuHandler(CurrentMenu position)
     {
+        ClearConsole();
+        CurrentMenu menuPosition = position;
+        switch (menuPosition)
+        {
+            case CurrentMenu.MainMenu:
+                ShowMenu(_mainMenu);
+                break;
+            case CurrentMenu.CarMenu:
+                ShowMenu(_clientList.Clients);
+                break;
+            case CurrentMenu.PartMenu:
+                ShowMenu(_clientList.Clients[_menuObject].CarEquipment);
+                break;
+            case CurrentMenu.StorageMenu:
+                ShowMenu(_storage.Stock);
+                break;
+        }
+    }
+
+    private void ShowMenu<T>(List<T> list)
+    {
+        _menuIndexUpper = list.Count;
+        for (int i = 0; i < _menuIndexUpper; i++)
+        {
+            WriteLine(_menuObject == i 
+                ? $">>{list[i]}<<"
+                : $"{list[i]}");
+        }
+    }
+
+    private void SwitchMenu(CurrentMenu position)
+    {
+        //CurrentMenu menuPosition = position;
         ConsoleKeyInfo userInput = ReadKey();
         switch (userInput.Key)
         {
             case ConsoleKey.UpArrow:
-                _currentMenuIndex = (_currentMenuIndex == 0) ? _menuIndexUpper - 1 : --_currentMenuIndex;
+                _menuObject = (_menuObject == 0) ? _menuIndexUpper - 1 : --_menuObject;
                 break;
             
             case ConsoleKey.DownArrow:
-                _currentMenuIndex = (_currentMenuIndex == _menuIndexUpper - 1) ? _menuIndexLower : ++_currentMenuIndex;
+                _menuObject = (_menuObject == _menuIndexUpper - 1) ? _menuIndexLower : ++_menuObject;
                 break;
             
             case ConsoleKey.Enter:
-                Type TypeOfT = typeof(T);
-                if (TypeOfT == typeof(string))
+                switch (position)
                 {
-                    PressEnterMainMenu();
-                }
-                else if (TypeOfT == typeof(Car))
-                {
-                    ShowParticularCar();
-                }
-                else if (TypeOfT == typeof(CarPart))
-                {
-                    RepairCarPart();
+                    case CurrentMenu.MainMenu:
+                        MainMenuAction();
+                        break;
+                    
+                    case CurrentMenu.CarMenu:
+                        ShowParticularCar();
+                        break;
+                    
+                    case CurrentMenu.PartMenu:
+                        RepairCarPart();
+                        break;
+                    
+                    case CurrentMenu.StorageMenu:
+                        //replace Car Part
+                        break;
                 }
                 break;
         }
     }
 
-    private void PressEnterMainMenu()
+    private void MainMenuAction()
     {
-        string chosenMenuIndex = _menuOptions[_currentMenuIndex];
+        string chosenMenuIndex = _mainMenu[_menuObject];
         if (_menuActions.TryGetValue(chosenMenuIndex.ToString(), out MenuAction action))
         {
             action.Invoke();
@@ -102,25 +133,27 @@ public class Menu
     {
         ClearConsole();
         WriteLine(CarToRepair);
-        WriteLine(CarToRepair.CarEquipment[_currentMenuIndex]);
+        WriteLine(CarToRepair.CarEquipment[_menuObject]);
         WriteLine("Choose the part to replace from storage:");
-        ShowMenu(_storage.Stock);
+        MenuHandler(CurrentMenu.StorageMenu);
+        SwitchMenu(CurrentMenu.StorageMenu);
     }
 
     private void ShowParticularCar()
     {
         ClearConsole();
         WriteLine(CarToRepair);
-        ShowMenu(_street.Clients[_currentMenuIndex].CarEquipment);
+        MenuHandler(CurrentMenu.PartMenu);
+        SwitchMenu(CurrentMenu.PartMenu);
         
     }
     private void StartWork()
     {
         WriteLine("These are clients for today:");
-        ShowMenu(_street.Clients);
-        SwitchMenu(_street.Clients);
+        MenuHandler(CurrentMenu.CarMenu);
+        SwitchMenu(CurrentMenu.CarMenu);
         WriteLine("Choose client to serve by pressing UP and DOWN arrows.");
-        //Thread.Sleep(7000);
+        Thread.Sleep(7000);
     }
 
     private void ShowStorage()
